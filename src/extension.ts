9fc759fd6b1fa2d73eccae942a0cda2fa331fa26
野迂迂 <yangwei1@outlook.com>
2023-06-12 12:00:37 +0800

    fix: remove hello world

--- src/extension.ts
+++ src/extension.ts
@@ -12,17 +12,6 @@ export function activate(context: vscode.ExtensionContext) {
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "vscode-plugin-brkill" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("vscode-plugin-brkill.helloWorld", () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from vscode-plugin-brkill!");
-  });
-
-  context.subscriptions.push(disposable);
-
   const rootPath = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0 ? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
 
   const treeData = new TreeDataProviderClass(rootPath);
